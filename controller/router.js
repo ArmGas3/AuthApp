d013333b790d74e2a221aafcff57f12ea0ee8e55
Rob Gas <robgas45@gmail.com>
2019-07-02 11:47:01 +0400

    Login Implemented

--- controller/router.js
+++ controller/router.js
@@ -7,11 +7,23 @@ router.get('/', (req, res) => {
     res.render('home');
 });
 
+router.get('/login', (req, res) => {
+    res.redirect('/');
+});
+
 router.post('/login', (req, res) => {
 
-    UserLoginModel.find({login: req.body.login, pass: req.body.pass}).then(data => {
-        console.log(data, 'You are logged in boss...');
-        res.render('home', {msg: 'logged'});
+    let {login, pass} = req.body;
+
+    UserLoginModel.find({login, pass}).then(data => {
+
+        if (data[0].login === login && data[0].pass === pass) {
+            res.render('login', {user: data[0].login});
+        } else {
+            res.redirect('/error');
+        }
+
+
     });
 
 
@@ -28,6 +40,10 @@ router.post('/register', (req, res) => {
 
 router.get('/index', (req, res) => {
     UserLoginModel.find({}).then(data => res.send(data));
+});
+
+router.get('/error', (req, res) => {
+    res.render('error');
 });
 
 module.exports = router;
